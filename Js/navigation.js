834aef694b1d2bdfeef0fa1cdcc0900c18a9f3b3
WillowGamesStudio <119956097+WillowGamesStudio@users.noreply.github.com>
2023-10-30 13:09:34 +0100

    Add files via upload

---
+++ Js/navigation.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+}
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
